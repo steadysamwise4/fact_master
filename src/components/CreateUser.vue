cbd7cf23718a9f82f92a4e157168e6240cdbc92c
steadysamwise4 <davenportsam44@gmail.com>
2025-10-29 12:12:32 -0500

    configuration of user data shape

--- src/components/CreateUser.vue
+++ src/components/CreateUser.vue
@@ -32,9 +32,48 @@ const handleAddUser = async () => {
     await loadProblems();
   }
   console.log(username.value);
+  const multData = constructUserData(multProblems.value);
+  const divData = constructUserData(divProblems.value);
   console.log('multProblems', multProblems.value);
   console.log('divProblems', divProblems.value);
-  addUser({ name: username.value });
+  addUser({ name: username.value, multData, divData });
   username.value = '';
 };
+
+const constructUserData = (problemSet) => {
+  const problemObj = {};
+  problemSet.forEach((prob) => {
+    problemObj[prob.id] = {
+      answerTimes: [20, 20, 20, 20, 20],
+      avgTime: 20,
+      mastered: false,
+    };
+  });
+  return problemObj;
+};
+
+// TODO: Figure out where this lives
+function updateProblemStats(problemData, problemId, newTime) {
+  const problem = problemData[problemId];
+
+  // Update times
+  problem.answerTimes.shift();
+  problem.answerTimes.push(newTime);
+
+  // Recalculate average
+  problem.avgTime =
+    problem.answerTimes.reduce((a, b) => a + b) / problem.answerTimes.length;
+
+  // Update mastery status
+  problem.mastered = problem.avgTime < 3; // or whatever your threshold is
+}
+
+// To derive total count TODO: Figure out where this lives
+function getMasteredCount(problemData) {
+  return Object.values(problemData).filter((p) => p.mastered).length;
+}
+
+// Usage:
+const multMastered = getMasteredCount(user.multData);
+const divMastered = getMasteredCount(user.divData);
 </script>
